1b7fd391d76eaaa35de948962a869160bd51e0ef
ayush-989 <ayusht9189@gmail.com>
2025-12-10 14:57:23 +0530

    Today I complete the introduction of javascrippt

---
+++ introduction/index.js
@@ -0,0 +1,107 @@
+console.log("Basic Print method in JavaScript");
+
+// Variable declaration
+let c, d, e;
+
+// Assign value to the variable
+c = 5; 
+
+// Computer value of variables
+d = c;
+e = c / d;
+//JavaScript Literals
+let num1 = 50
+let num2 = 50.05
+
+let str1 = "Geek"
+let str2 = 'Geeks'
+
+console.log(num1)
+console.log(num2)
+console.log(str1)
+console.log(str2)
+//JavaScript Variables
+// Declare a variable and initialize it
+// Global variable declaration
+let Name = "Apple";
+
+// Function definition
+function MyFunction() {
+
+    // Local variable declaration
+    let num = 45;
+
+    // Display the value of Global variable
+    console.log(Name);
+
+    // Display the value of local variable
+    console.log(num);
+}
+
+// Function call
+MyFunction();
+
+//JavaScript Operators
+// Variable Declarations
+let x, y, sum;
+
+// Assign value to the variables
+x = 3;
+y = 23;
+
+// Use arithmetic operator to
+// add two numbers
+sum = x + y;
+
+console.log(sum);
+
+//JavaScript Expressions
+// Variable Declarations
+//let x, num, sum;
+
+// Assign value to the variables
+x = 20;
+y = 30
+
+// Expression to divide a number
+num = x / 2;
+
+// Expression to add two numbers
+sum = x + y;
+
+console.log(num + "\n" + sum);
+
+//JavaScript Comments
+// Variable Declarations
+//let x, num, sum;
+
+// Assign value to the variables
+x = 20;
+y = 30
+
+/* Expression to add two numbers */
+sum = x + y;
+
+console.log(sum);
+
+//  JavaScript Data Types
+// Function definition
+function func() {
+
+    // Declare a variable
+    let num = 45;
+
+    // Display the result
+    console.log(num);
+}
+
+// Function call
+func();
+
+
+// javascript case sensitivity
+let firstName = "Geek";
+let firstname = 100;
+
+console.log(firstName);
+console.log(firstname);
