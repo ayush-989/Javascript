3b0bf856933f49dcd756d144311554e76665c366
ayush-989 <ayusht9189@gmail.com>
2025-12-16 19:15:59 +0530

    JavaScript Variable

---
+++ JavaScript Variables and Data type/JsVariables.js
@@ -0,0 +1,17 @@
+// old style
+var a =10;
+console.log(a);
+
+// Prferred for non-const
+let = 20;
+
+console.log(b);
+
+// Preferred for constant values
+const c = 30;
+console.log(c);
+
+// Naming Variables
+let userName = "John"; // camelCase
+let user_name = "John"; // snake_case
+let UserName = "John"; // PascalCase
