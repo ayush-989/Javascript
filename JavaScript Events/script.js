2757781388dc36bdce99d382cee773bd45ac21d4
ayush-989 <ayusht9189@gmail.com>
2025-12-16 13:54:17 +0530

    JavaScript Event

---
+++ JavaScript Events/script.js
@@ -0,0 +1,9 @@
+// Event Propagation in JavaScript
+document.querySelector("div").addEventListener("click", () => {
+    console.log("Div clicked");
+}, true); // Capturing phase
+
+button.addEventListener("click", (e) => {
+    console.log("Button clicked");
+    e.stopPropagation(); // Stops propagation
+});
