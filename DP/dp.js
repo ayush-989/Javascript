529b486102e422de06a392609494790aaf48838a
ayush-989 <ayusht9189@gmail.com>
2026-01-09 17:36:40 +0530

    java

--- DP/dp.js
+++ DP/dp.js
@@ -9,17 +9,4 @@ var maxDotProduct = function(nums1, nums2) {
         Array(m + 1).fill(NEG_INF)
     );
 
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= m; j++) {
-            const product = nums1[i - 1] * nums2[j - 1];
-
-            dp[i][j] = Math.max(
-                product + Math.max(dp[i - 1][j - 1], 0), // take pair
-                dp[i - 1][j],                            // skip nums1
-                dp[i][j - 1]                             // skip nums2
-            );
-        }
-    }
-
-    return dp[n][m];
 };
