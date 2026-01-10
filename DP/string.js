e71c0334fd65a333fb42c34b69b7cbaef6de2013
ayush-989 <ayusht9189@gmail.com>
2026-01-10 22:51:04 +0530

    javascript

--- DP/string.js
+++ DP/string.js
@@ -12,4 +12,24 @@ var minimumDeleteSum = function(s1, s2) {
         dp[i][0] = dp[i - 1][0] + s1.charCodeAt(i - 1);
     }
 
- 
+    // Base case: delete all characters from s2
+    for (let j = 1; j <= m; j++) {
+        dp[0][j] = dp[0][j - 1] + s2.charCodeAt(j - 1);
+    }
+
+    // Fill DP table
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            if (s1[i - 1] === s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.min(
+                    dp[i - 1][j] + s1.charCodeAt(i - 1),
+                    dp[i][j - 1] + s2.charCodeAt(j - 1)
+                );
+            }
+        }
+    }
+
+    return dp[n][m];
+};
